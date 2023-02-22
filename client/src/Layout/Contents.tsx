273d12a207b4aab18fe3b928a5f193d66430ada6
falconlee236 <falconlee236@naver.com>
2023-02-23 00:30:06 +0900

    load react-md-editor

--- client/src/Layout/Contents.tsx
+++ client/src/Layout/Contents.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import MDEditor from '@uiw/react-md-editor';
 import Topbar, { OpenStateProps } from '../Component/Topbar';
 
+// {openState} : {openState : 타입}
 export default function Contents({ openState }: OpenStateProps) {
+  const [value, setValue] = useState<string | undefined>('**Hello world!!!**');
   return (
     <ContentsDiv isOpen={openState.open}>
       <Topbar openState={openState} />
+      <div className="container">
+        <MDEditor value={value} onChange={(val) => setValue(val)} />
+        <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} />
+      </div>
     </ContentsDiv>
   );
 }
